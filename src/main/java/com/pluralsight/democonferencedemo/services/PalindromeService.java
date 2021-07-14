c472470e26735bb6cdbbe780f11253192ae562e5
skruddy <skruddy@gmail.com>
2021-07-14 14:15:27 +0530

    Commit for Sonar

--- src/main/java/com/pluralsight/democonferencedemo/services/PalindromeService.java
+++ src/main/java/com/pluralsight/democonferencedemo/services/PalindromeService.java
@@ -4,16 +4,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PalindromeService {
-	
+
 	public boolean isPalindrome(String inputString) {
-	    if (inputString.length() == 0) {
-	        return true;
-	    } else {
-	        char firstChar = inputString.charAt(0);
-	        char lastChar = inputString.charAt(inputString.length() - 1);
-	        String mid = inputString.substring(1, inputString.length() - 1);
-	        return (firstChar == lastChar) && isPalindrome(mid);
-	    }
+		System.out.println("String ->" + inputString);
+		if (inputString.length() == 0) {
+			return true;
+		} else {
+			char firstChar = inputString.charAt(0);
+			char lastChar = inputString.charAt(inputString.length() - 1);
+			String mid = inputString.substring(1, inputString.length() - 1);
+			return (firstChar == lastChar) && isPalindrome(mid);
+		}
+
 	}
 
 }
